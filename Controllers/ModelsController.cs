8b7087bb7aa07311e14186d1cb5eadbe65f179c4
abdelrhman abdallat <abd.abdallat@hotmail.com>
2023-12-31 12:47:40 +0300

    integration Service

--- Controllers/ModelsController.cs
+++ Controllers/ModelsController.cs
@@ -7,5 +7,18 @@ namespace CarseerExam.Controllers
     [ApiController]
     public class ModelsController : Controller
     {
+
+        public ModelsController()
+        {
+            
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetModels([FromQuery] int modelYear, [FromQuery] string make)
+        {
+
+
+            return Ok();
+        }
     }
 }
